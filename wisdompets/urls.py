9e200d6737bcfc3cb91197a7b32df1df5c27414f
Recca789 <pawczyk81994@gmail.com>
2019-11-15 10:04:56 +0100

    Initial commit

---
+++ wisdompets/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, re_path
+
+from adoptions import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^$', views.home, name='home'),
+    re_path(r'^adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
+]
